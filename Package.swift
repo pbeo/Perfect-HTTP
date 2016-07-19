8da3e343876429861d7b4e36e98843329979538c
Piotr Ożóg <pozog@pgs-soft.com>
2016-07-19 14:32:07 +0200

    change package

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "PerfectHTTP",
 	targets: [],
-	dependencies: [.Package(url: "https://github.com/pbeo/Perfect.git", versions: Version(0,0,0)..<Version(10,0,0))],
+	dependencies: [],
 	exclude: []
 )
