98571badced4a6a208aae649cb7816a7d3e4ff69
Piotr Ożóg <pozog@pgs-soft.com>
2016-07-19 12:38:53 +0200

    change urls

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
 	name: "PerfectHTTP",
 	targets: [],
-	dependencies: [.Package(url: "https://github.com/PerfectlySoft/Perfect.git", versions: Version(0,0,0)..<Version(10,0,0))],
+	dependencies: [.Package(url: "https://github.com/pbeo/Perfect.git", versions: Version(0,0,0)..<Version(10,0,0))],
 	exclude: []
 )
